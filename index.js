7fdce4d59b304a65e6ce399b0655f6538818fa20
Andy Piper <andypiperuk@gmail.com>
2014-02-11 15:00:07 +0000

    just some fun

---
+++ index.js
@@ -0,0 +1,34 @@
+var api = require('marvel-api');
+var settingsFile = "./settings";
+var settings = require(settingsFile);
+
+var marvel = api.createClient({
+  publicKey: settings.publicKey,
+  privateKey: settings.privateKey
+});
+
+var numComics, randComic, randOffset = 0;
+
+marvel.comics.findAll(1, function(err, results) {
+  if (err) {
+    return console.error(err);
+  }
+
+  numComics = (results.meta.total);
+  randComic = Math.floor(Math.random() * numComics) + 1;
+  randOffset = Math.ceil((randComic+1)/10)*10;
+
+  marvel.comics.findAll(1, randOffset, function(err, results) {
+    if (err) {
+      return console.error(err);
+    }
+
+    console.log(results.data[0].title);
+    console.log(results.data[0].description);
+    console.log(results.data[0].thumbnail.path + "/portrait_uncanny." + results.data[0].thumbnail.extension);
+  });
+
+});
+
+
+
---
+++ settings.js
@@ -0,0 +1,5 @@
+module.exports = {
+    // Marvel API keys (via https://developer.marvel.com/account)
+    publicKey: 'yourpublickey',
+    privateKey: 'yourprivatekey'
+}
